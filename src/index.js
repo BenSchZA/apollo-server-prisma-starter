ee6a842359f9fe4f40bfa33d937a79b5a6c2968e
BenSchZA <bscholtz.bds@gmail.com>
2019-05-04 22:30:06 +0200

    Add  basic auth config from tutorial

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
+const jwt = require('jsonwebtoken');
+
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
 const { prisma } = require('./generated/prisma-client/index');
 
+const getUser = token => {
+  try {
+    if (token) {
+      return jwt.verify(token, 'my-secret-from-env-file-in-prod')
+    }
+    return null
+  } catch (err) {
+    return null
+  }
+}
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {
-    prisma
-  }
-});
+  context: ({ req }) => {
+    const tokenWithBearer = req.headers.authorization || ''
+    const token = tokenWithBearer.split(' ')[1]
+    const user = getUser(token)
+
+    return {
+      user,
+      prisma, // the generated prisma client if you are using it
+    }
+  },
+})
 
 server
   .listen({
