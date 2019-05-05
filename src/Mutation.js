5e0900f594d65984e21b65593159232181592035
BenSchZA <bscholtz.bds@gmail.com>
2019-05-05 18:33:54 +0200

    Extract generateToken function

--- src/Mutation.js
+++ src/Mutation.js
@@ -1,24 +1,28 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+let generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      username: user.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30d', // token will expire in 30days
+    },
+  )
+};
+
 const Mutation = {
   register: async (parent, { username, password }, ctx, info) => {
     const hashedPassword = await bcrypt.hash(password, 10)
     const user = await ctx.prisma.createUser({
       username,
       password: hashedPassword,
-    })
+    });
 
-    const token = jwt.sign(
-      {
-        id: user.id,
-        username: user.email,
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: '30d', // token will expire in 30days
-      },
-    )
+    const token = generateToken(user);
     return {
       token,
       user,
@@ -37,16 +41,7 @@ const Mutation = {
       throw new Error('Invalid Login');
     }
 
-    const token = jwt.sign(
-      {
-        id: user.id,
-        username: user.email,
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: '30d', // token will expire in 30days
-      },
-    )
+    const token = generateToken(user);
     return {
       token,
       user,
